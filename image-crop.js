5304ab3e381ec60f0c9561cf1cb0ad9e2c88c911
Antonio Marcos SS Jr <antonio@speedysolutions.com.br>
2015-07-09 16:30:00 -0300

    now the background will show only when is step 2.

--- image-crop.js
+++ image-crop.js
@@ -762,7 +762,7 @@
     .directive('imageCrop', function() {
 
       return {
-        template: '<div id="image-crop-{{ rand }}" class="ng-image-crop ng-image-crop--{{ shape }}" ng-style="moduleStyles"><section ng-style="sectionStyles" ng-show="step==1"><div style="margin-bottom: 15px;" class="row no-margin-right-s-only no-margin-left-s-only">        <div class="file-field input-field btn waves-effect waves-light col s12 m10 offset-m1 l8 offset-l2"><span>Selecionar		<input type="file" class="image-crop-input"  accept="{{accept}}" />         </span></div>			</div></section><section ng-style="sectionStyles" ng-show="step==2"><canvas class="cropping-canvas" width="{{ canvasWidth }}" height="{{ canvasHeight }}" ng-mousemove="onCanvasMouseMove($event)" ng-mousedown="onCanvasMouseDown($event)" ng-mouseup="onCanvasMouseUp($event)"></canvas><div ng-style="croppingGuideStyles" class="cropping-guide"></div><div class="zoom-handle" ng-mousemove="onHandleMouseMove($event)" ng-mousedown="onHandleMouseDown($event)" ng-mouseup="onHandleMouseUp($event)"><span>&larr; zoom &rarr;</span></div><button  class="btn green lighten-2 col s12 m10 offset-m1 l8 offset-l2" ng-click="crop()" style="margin-bottom: 20px;">Recortar</button></section><section ng-style="sectionStyles" class="image-crop-section-final" ng-show="step==3"><img class="image-crop-final" ng-src="{{ croppedDataUri }}" /></section></div>',
+        template: '<div id="image-crop-{{ rand }}" class="ng-image-crop step-{{step}}  ng-image-crop--{{ shape }}" ng-style="moduleStyles"><section ng-style="sectionStyles" ng-show="step==1"><div style="margin-bottom: 15px;" class="row no-margin-right-s-only no-margin-left-s-only">        <div class="file-field input-field btn waves-effect waves-light col s12 m10 offset-m1 l8 offset-l2"><span>Selecionar		<input type="file" class="image-crop-input"  accept="{{accept}}" />         </span></div>			</div></section><section ng-style="sectionStyles" ng-show="step==2"><canvas class="cropping-canvas" width="{{ canvasWidth }}" height="{{ canvasHeight }}" ng-mousemove="onCanvasMouseMove($event)" ng-mousedown="onCanvasMouseDown($event)" ng-mouseup="onCanvasMouseUp($event)"></canvas><div ng-style="croppingGuideStyles" class="cropping-guide"></div><div class="zoom-handle" ng-mousemove="onHandleMouseMove($event)" ng-mousedown="onHandleMouseDown($event)" ng-mouseup="onHandleMouseUp($event)"><span>&larr; zoom &rarr;</span></div><button  class="btn green lighten-2 col s12 m10 offset-m1 l8 offset-l2" ng-click="crop()" style="margin-bottom: 20px;">Recortar</button></section><section ng-style="sectionStyles" class="image-crop-section-final" ng-show="step==3"><img class="image-crop-final" ng-src="{{ croppedDataUri }}" /></section></div>',
         replace: true,
         restrict: 'AE',
         scope: {
@@ -913,8 +913,9 @@
 
             // console.log('canvas width', $canvas.width);
             // console.log('image width', imgWidth);
-
             maxZoomedInLevel = ($canvas.width - 100) / imgWidth;
+			
+			maxZoomedInLevel = ($canvas.height - 100) / imgHeight;
             // console.log('maxZoomedInLevel', maxZoomedInLevel);
 
             maxZoomGestureLength = to2Dp(Math.sqrt(Math.pow($canvas.width, 2) + Math.pow($canvas.height, 2)));
